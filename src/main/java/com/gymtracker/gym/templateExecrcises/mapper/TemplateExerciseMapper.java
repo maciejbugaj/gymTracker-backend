d1a5e5ced5fae4012979567ef4035ad02f37b014
Luzu4 <maciekluz@gmail.com>
2026-06-08 22:56:33 +0200

    removed unused import

--- src/main/java/com/gymtracker/gym/templateExecrcises/mapper/TemplateExerciseMapper.java
+++ src/main/java/com/gymtracker/gym/templateExecrcises/mapper/TemplateExerciseMapper.java
@@ -3,7 +3,6 @@ package com.gymtracker.gym.templateExecrcises.mapper;
 import com.gymtracker.gym.templateExecrcises.dto.TemplateExerciseResponse;
 import com.gymtracker.gym.templateExecrcises.model.TemplateExercise;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TemplateExerciseMapper {
